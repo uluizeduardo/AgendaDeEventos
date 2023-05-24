e394039f83a87801ca59aa7822896fdb145a7273
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-05-23 21:56:19 -0300

    FEATURE/CREAT-0001/v2023.0001 - Criado Projeto

---
+++ agenda-de-eventos/src/test/java/br/com/agendadeeventos/AgendaDeEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agendadeeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaDeEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
