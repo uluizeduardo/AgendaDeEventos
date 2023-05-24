6eb2b35b3303a8d7a0cb97cdae5d0dc7fd751258
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-05-23 22:56:12 -0300

    FEATURE/CREAT-0001/v2023.0001 - Criada a estrutura de pastas do projeto

--- agenda-de-eventos/src/main/java/br/com/agendadeeventos/AgendaDeEventosApplication.java
+++ agenda-de-eventos/src/main/java/br/com/agendadeeventos/AgendaDeEventosApplication.java
@@ -8,6 +8,7 @@ public class AgendaDeEventosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaDeEventosApplication.class, args);
+		System.out.println("Hello world");
 	}
 
 }
